40d2bd68c248be276430187cc73f416f928237e5
Michael Godfrey <m.godfrey59@gmail.com>
2022-01-03 21:32:32 +1100

    added Fiji project and made message service show loading animation do auto return

--- js/message.js
+++ js/message.js
@@ -2,6 +2,7 @@
 const sender = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
+const loading = document.getElementById('loading');
 
 // Send email function from https://www.emailjs.com/
 window.onload = function() {
@@ -10,15 +11,16 @@ window.onload = function() {
         if (sender.value === '' || email.value === '' || message.value === '') {
           alert('Please complete all fields before sending message.');
         } else {
+            loading.style.display = "flex";
           // log in https://dashboard.emailjs.com to get service id and user id
-          emailjs.sendForm('service_4dftmcc', 'contact_form', this)
+            emailjs.sendForm('service_4dftmcc', 'contact_form', this)
               .then(function() {
                   alert('Sent! Thank you for the message, I will get back to you shortly.');
-                  sender.value = '';
-                  email.value = '';
-                  message.value = '';
+                  loading.style.display = "none";
+                  location.href = "index.html";
               },  function(error) {
                   alert('Well this is embarrassing :( Sorry about that. Please contact me through social media instead.', error);
+                  location.href = "index.html";
               });
         }
     });
