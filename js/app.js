10ab2f72417629e115aedcacb573ff0554d533b9
Michael Godfrey <m.godfrey59@gmail.com>
2022-03-05 21:26:33 +1100

    maintenance

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Dropdown menu for mobile devices
-// Shout out to Web Dev Simplified for guide on dropdown menu 
+// Shout out to Web Dev Simplified for guide on dropdown menu
 // video: https://www.youtube.com/watch?v=S-VeYcOCFZw&ab_channel=WebDevSimplified
-document.addEventListener("click", e => {
+document.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
   if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
@@ -11,21 +11,21 @@ document.addEventListener("click", e => {
     currentDropdown.classList.toggle("active");
   }
 
-  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
     if (dropdown === currentDropdown) return;
     dropdown.classList.remove("active");
   });
 });
 
-// hide and show button
+// hide and show button in about section
 function hideShow() {
   const moreInfo = document.getElementById("more-info");
   const toggleBtn = document.getElementById("toggle-hide-show");
-  if (moreInfo.style.display === "none") {
-    moreInfo.style.display = "flex";
+  if (!moreInfo.classList.contains("show")) {
+    moreInfo.classList.add("show");
     toggleBtn.innerHTML = "Show Less &uarr;";
   } else {
-    moreInfo.style.display = "none";
+    moreInfo.classList.remove("show");
     toggleBtn.innerHTML = "Show More &darr;";
   }
 }
