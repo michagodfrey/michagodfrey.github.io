3a8f0196ef7e9fc07a0861d3cf8201c512227594
Michael Godfrey <m.godfrey59@gmail.com>
2022-08-25 20:57:30 +1200

    updated  about section text

--- js/app.js
+++ js/app.js
@@ -62,13 +62,9 @@ function reveal() {
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     }
-    // else {
-    //   reveals[i].classList.remove("active");
-    // }
   }
 }
 
 window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
 reveal();
