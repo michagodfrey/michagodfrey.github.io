2098ebe9e7dc0b37c46a36c29bb9ddfa9f9d6c9a
Michael Godfrey <m.godfrey59@gmail.com>
2023-05-23 11:05:06 +1200

    styled project to standard cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
-import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
-
-    const isLargeScreen = useMediaQuery({ query: '(min-width: 1024px' });
-    const offsetValue = isLargeScreen ? 0 : -110;
 
     return (
         <div name="home" className="h-screen py-8">
             <div className="max-w-screen-xl mx-auto px-8 lg:px-36 flex flex-col justify-center h-full w-full">
                 <p className="text-emerald-600">Hi, my name is</p>
                 <h1 className="text-4xl sm:text-7xl text-[#ccd6f6]">Michael Godfrey</h1>
-                <p className="text-2xl sm:text-5xl text-[#8892b0]">I'm a frontend web developer</p>
+                <p className="text-2xl sm:text-5xl text-[#8892b0]">I'm a web developer</p>
                 <p className="py-4 max-w-[600px] text-[#8892b0]">
                     Welcome and thank you for visiting my portfolio. 
                     Here I showcase some selected projects and provide links to my social media pages.
@@ -21,7 +17,7 @@ const Home = () => {
                 </p>
                 <div>
                     <button>
-                        <Link to="work" smooth={true} duration={500} offset={offsetValue} className="text-white border-2 px-6 py-3 my-2 rounded-lg flex items-center group hover:bg-emerald-600 hover:border-emerald-600">
+                        <Link to="work" smooth={true} duration={500} offset={-96} className="text-white border-2 px-6 py-3 my-2 rounded-lg flex items-center group hover:bg-emerald-600 hover:border-emerald-600">
                             See Work
                             <span className="ml-3 group-hover:rotate-90 duration-300">
                                 <HiArrowNarrowRight />
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,7 @@ const Work = () => {
 
     return (
         <div name="work" className="min-h-screen py-8"> 
-            <div className="max-w-screen-xl mx-auto px-8 lg:px-36 flex flex-col w-full h-full"> 
+            <div className="max-w-screen-xl mx-auto px-8 lg:px-36 flex flex-col justify-center w-full h-full"> 
                 <div>
                     <h2 className="text-4xl inline border-b-4 border-emerald-600">Work</h2>
                      <p className="py-4">Tap or hover over the images for more information and links</p>
@@ -16,25 +16,19 @@ const Work = () => {
                 <div className="grid md:grid-cols-2 gap-4">
                     {projectData.map((project) => {
                         return (
-                            <div key={project.title} className="group relative shadow-lg shadow-[#040c16] rounded-lg mx-auto">
-                                <img src={project.image} alt={project.title} className="rounded-lg" />
-                                <div className="absolute h-[33%] -translate-y-[100%] overflow-hidden group-hover:h-[100%] duration-500 bg-black/90 w-full rounded-b-lg group-hover:rounded-lg flex flex-col p-4">
-                                <h3 className="mb-[15%] mt-[5%] text-xl sm:text-2xl lg:text-3xl text-white tracking-wider border-b-4 w-fit">
-                                {project.title}
-                                </h3>
-                                <div>
-                                    <span className="text-white lg:text-lg xl:text-xl">
-                                        {project.caption}
-                                    </span>
-                                    <div className="mt-[5%]">
-                                        <a href={project.demo} target="_blank" rel="noreferrer">
-                                            <button className="text-center rounded-lg px-4 py-1 lg:py-3 my-2 bg-white text-gray-700 text-lg hover:bg-emerald-600 hover:text-white">Demo</button>
-                                        </a>
-                                        <a href={project.code} target="_blank" rel="noreferrer">
-                                            <button className="text-center rounded-lg px-4 py-1 lg:py-3 ml-2 lg:ml-4 border text-white text-lg hover:bg-emerald-600 hover:border-emerald-600">Code</button>
-                                        </a>
-                                    </div>
-                                </div>
+                            <div key={project.title} class="shadow-[#040c16] mx-auto">
+                                <a href={project.demo} target="_blank" rel="noreferrer" >
+                                    <img src={project.image} alt={project.title} class="rounded-t-lg border-t border-x" />
+                                </a>
+                                <div class="p-4 pb-8 rounded-b-lg border-b border-x">
+                                    <h3 class="mb-2 text-2xl lg:text-3xl text-[#ccd6f6]">{project.title}</h3>
+                                    <p class="mb-6 text-white lg:text-lg xl:text-xl">{project.caption}</p>
+                                    <a href={project.demo} target="_blank" rel="noreferrer" class="text-center rounded-lg px-4 py-3 my-2 bg-white text-gray-700 text-lg hover:bg-emerald-600 hover:text-white">
+                                       Demo
+                                    </a>
+                                    <a href={project.code} target="_blank" rel="noreferrer" class="text-center rounded-lg px-4 py-3 ml-2 lg:ml-4 border text-white text-lg hover:bg-emerald-600 hover:border-emerald-600">
+                                       Code
+                                    </a>
                                 </div>
                             </div>
                         )
